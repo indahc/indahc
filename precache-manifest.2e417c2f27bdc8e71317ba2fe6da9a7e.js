587e2ae07582e5686cabd67269f15e39848cb982
Indah Cahyani <indahcahyani.279@gmail.com>
2020-09-18 02:09:15 +0700

    first commit

---
+++ precache-manifest.2e417c2f27bdc8e71317ba2fe6da9a7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce665cf8f70ab5dfebe1c5b5c2bc90a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3022ce629976accbcf4c",
+    "url": "/static/css/main.d340197c.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3022ce629976accbcf4c",
+    "url": "/static/js/main.4e0a3847.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "493470b74c9a5ea340dbb2a63e97ad27",
+    "url": "/static/media/img.493470b7.svg"
+  }
+]);
